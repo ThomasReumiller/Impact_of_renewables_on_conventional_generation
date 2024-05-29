b27c3e17e678d1fa1797e3b9b57bddebaae83e6d
ThomasReumiller <30797615+ThomasReumiller@users.noreply.github.com>
2024-05-29 10:37:11 +0200

    Add files via upload

---
+++ R/package_info.r
@@ -0,0 +1,9 @@
+ps.pkg.info = function() {
+  # Name of this package
+  package = "RTutorImpactOfRenewablesOnConventionalGeneration"
+  
+  # Name of problem sets in the package
+  ps = c("Impact_of_renewables_on_conventional_generation")
+  
+  list(package=package, ps = ps)
+}
